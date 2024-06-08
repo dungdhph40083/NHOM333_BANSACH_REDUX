98e6a44ecd6da8d1bc709c891cb3039f26a8ee8f
dungdhph40083 <147228552+dungdhph40083@users.noreply.github.com>
2024-06-08 18:42:51 +0700

    Initial upload (3)
    No dupes pls

---
+++ AppData/CartDetails.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace AppData
+{
+    public class CartDetails
+    {
+        [Key]
+        public Guid CartDetailsID { get; set; }
+        public string? BookID { get; set; }
+        [ForeignKey("Username")]
+        public string Username { get; set; } = null!;
+        public int Quantity { get; set; }
+        public int Status { get; set; }
+        public virtual Cart? Cart { get; set; }
+        public virtual Book? Book { get; set; }
+    }
+}
